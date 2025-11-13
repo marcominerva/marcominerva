a5fa633ad461412c4a3e6a595a3fd3967cdb8f66
Marco Minerva <marco.minerva@gmail.com>
2025-11-13 17:09:59 +0100

    Add files via upload

---
+++ PrintDate.cs
@@ -0,0 +1,10 @@
+#:package Spectre.Console@0.54.*
+
+using Spectre.Console;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var formatted = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+var figlet = new FigletText(formatted).Color(Color.White) .Centered();               
+
+AnsiConsole.Write(figlet);
