a07efcecc98eda9b931cec2228a7be3c9b3c5e7a
Marco Minerva <marco.minerva@gmail.com>
2025-11-28 09:21:27 +0100

    Update PrintDate.cs

--- PrintDate.cs
+++ PrintDate.cs
@@ -5,6 +5,7 @@ using Spectre.Console;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 var formatted = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-var figlet = new FigletText(formatted).Color(Color.White) .Centered();               
+var figlet = new FigletText(formatted).Color(Color.White).Centered();               
+
 
 AnsiConsole.Write(figlet);
